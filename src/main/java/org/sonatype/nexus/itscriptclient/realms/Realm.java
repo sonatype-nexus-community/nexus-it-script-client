bbe54e98ddd643dec9d255c5fb51143a4097ad5b
Mark Dodgson <mdodgson@sonatype.com>
2018-05-03 20:23:00 +0100

    removed unused jsonslurper from script

--- src/main/java/org/sonatype/nexus/itscriptclient/realms/Realm.java
+++ src/main/java/org/sonatype/nexus/itscriptclient/realms/Realm.java
@@ -4,10 +4,8 @@ import org.sonatype.nexus.itscriptclient.script.ScriptRunner;
 
 public class Realm
 {
-  static final String ENABLE_REALM = "import groovy.json.JsonSlurper" +
-      "\nimport org.sonatype.nexus.security.realm.RealmManager" +
-      "\n" +
-      "\nparsed_args = new JsonSlurper().parseText(args)" +
+  static final String ENABLE_REALM =
+      "import org.sonatype.nexus.security.realm.RealmManager" +
       "\n" +
       "\nrealmManager = container.lookup(RealmManager.class.getName())" +
       "\nrealmManager.enableRealm('%s', %b)";
